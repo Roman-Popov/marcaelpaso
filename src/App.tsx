fa8a6be86d9b9792d4d858003e5da441b6c4e13c
Roman Popov <popov.r.k@yandex.ru>
2024-09-17 12:22:38 +0500

    Update API token

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={process.env.PUBLIC_URL + 'logo512.png'} className="App-logo" alt="logo" />
-        <p>
-          Marca el Paso <br /> Студия танцев
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={`${process.env.PUBLIC_URL}logo512.png`} className="App-logo" alt="logo" />
+      <p>
+        Marca el Paso
+        {' '}
+        <br />
+        {' '}
+        Студия танцев
+        <br />
+        {process.env.REACT_APP_API_KEY}
+      </p>
+    </header>
+  </div>
+);
 
 export default App;
--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const IS_ENV_DEVELOPMENT = process.env.NODE_ENV === 'development';
 export const IS_ENV_PRODUCTION = process.env.NODE_ENV === 'production';
 export const IS_ENV_TEST = process.env.NODE_ENV === 'test';
 
-export const API_KEY = process.env.REACT_APP_API_KEY;
+export const API_KEY = String(process.env.REACT_APP_API_KEY_PART_1) + String(process.env.REACT_APP_API_KEY_PART_2);
 export const DOMAIN = process.env.REACT_APP_DOMAIN;
 export const PHONE_NUMBER = process.env.REACT_APP_PHONE_NUMBER;
 export const TELEGRAM_ID = process.env.REACT_APP_TELEGRAM_ID;
