45e714a166c2b15df7f9d28731521f9331f71829
Roman Popov <popov.r.k@yandex.ru>
2024-09-29 23:29:31 +0500

    Rework loaders at MobilePrices

--- src/modules/prices/mobile-prices/mobile-prices.tsx
+++ src/modules/prices/mobile-prices/mobile-prices.tsx
@@ -16,15 +16,9 @@ const MobilePrices = (props: MobilePricesProps) => {
 
   const { t } = useTranslation();
 
-  if (!pricesData) {
-    return (
-      <Box sx={{ px: 1 }}>
-        <NoDataBlock type={pricesData === null ? 'error' : 'loading'} />
-      </Box>
-    );
-  }
-
-  const { trial, oneTime, subscriptions } = pricesData;
+  const trial = pricesData?.trial;
+  const oneTime = pricesData?.oneTime;
+  const subscriptions = pricesData?.subscriptions;
 
   return (
     <Stack sx={{ px: { sm: 1 } }} spacing={2.5}>
@@ -32,56 +26,76 @@ const MobilePrices = (props: MobilePricesProps) => {
         <Typography fontWeight="bold" fontSize="1.15em" textAlign="center">
           {t('prices.singleVisit')}
         </Typography>
-        <Box pr={{ xs: 1, sm: 2 }}>
-          <Typography fontSize="0.6em" textAlign="right">{t('prices.price')}</Typography>
-        </Box>
-        <StyledCard variant="outlined">
-          <PricesRow title={t('prices.trial')} price={trial.price} />
-        </StyledCard>
-        <StyledCard
-          variant="outlined"
-          sx={(theme) => ({
-            borderColor: theme.palette.success.main,
-            '&:hover': {
-              boxShadow: `0 0 0 2px inset ${theme.palette.success.light}`,
-              ...(theme.palette.mode === 'dark'
-                ? { bgcolor: theme.palette.grey[900] }
-                : { }
-              ),
-            },
-          })}
-        >
-          <PricesRow title={t('prices.regular')} price={oneTime.price} />
-        </StyledCard>
-        <Typography fontSize="0.9em" sx={{ px: 1.5, pt: 0.5 }}>{t('prices.trialDetailed')}</Typography>
+        {trial && oneTime
+          ? (
+            <>
+              <Box pr={{ xs: 1, sm: 2 }}>
+                <Typography fontSize="0.6em" textAlign="right">{t('prices.price')}</Typography>
+              </Box>
+              <StyledCard variant="outlined">
+                <PricesRow title={t('prices.trial')} price={trial.price} />
+              </StyledCard>
+              <StyledCard
+                variant="outlined"
+                sx={(theme) => ({
+                  borderColor: theme.palette.success.main,
+                  '&:hover': {
+                    boxShadow: `0 0 0 2px inset ${theme.palette.success.light}`,
+                    ...(theme.palette.mode === 'dark'
+                      ? { bgcolor: theme.palette.grey[900] }
+                      : { }
+                    ),
+                  },
+                })}
+              >
+                <PricesRow title={t('prices.regular')} price={oneTime.price} />
+              </StyledCard>
+              <Typography fontSize="0.9em" sx={{ px: 1.5, pt: 0.5 }}>{t('prices.trialDetailed')}</Typography>
+            </>
+          )
+          : (
+            <Box sx={{ px: 1 }}>
+              <NoDataBlock type={pricesData === null ? 'error' : 'loading'} />
+            </Box>
+          )}
       </Stack>
       <Stack spacing={0.5}>
         <Typography fontWeight="bold" fontSize="1.15em" textAlign="center">
           {t('prices.subscriptions')}
         </Typography>
-        <Stack direction="row" spacing={2} sx={{ px: { xs: 1, sm: 2 }, justifyContent: 'end' }}>
-          <Typography fontSize="0.6em" sx={{ pr: { xs: 0, sm: 2 } }}>{t('prices.validity')}</Typography>
-          <Tablet>
-            <Box minWidth="9em">
-              <Typography fontSize="0.6em" textAlign="right">{t('prices.price')}</Typography>
+        {subscriptions
+          ? (
+            <>
+              <Stack direction="row" spacing={2} sx={{ px: { xs: 1, sm: 2 }, justifyContent: 'end' }}>
+                <Typography fontSize="0.6em" sx={{ pr: { xs: 0, sm: 2 } }}>{t('prices.validity')}</Typography>
+                <Tablet>
+                  <Box minWidth="9em">
+                    <Typography fontSize="0.6em" textAlign="right">{t('prices.price')}</Typography>
+                  </Box>
+                </Tablet>
+                <Mobile>
+                  <Box minWidth="5em">
+                    <Typography fontSize="0.6em" textAlign="right">{t('prices.price')}</Typography>
+                  </Box>
+                </Mobile>
+              </Stack>
+              {subscriptions.map((subscription, index) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <StyledCard key={index} variant="outlined">
+                  <PricesRow
+                    price={subscription.price}
+                    title={t('prices.amountCount', { count: subscription.amount })}
+                    validity={subscription.validity}
+                  />
+                </StyledCard>
+              ))}
+            </>
+          )
+          : (
+            <Box sx={{ px: 1 }}>
+              <NoDataBlock type={pricesData === null ? 'error' : 'loading'} />
             </Box>
-          </Tablet>
-          <Mobile>
-            <Box minWidth="5em">
-              <Typography fontSize="0.6em" textAlign="right">{t('prices.price')}</Typography>
-            </Box>
-          </Mobile>
-        </Stack>
-        {subscriptions.map((subscription, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <StyledCard key={index} variant="outlined">
-            <PricesRow
-              price={subscription.price}
-              title={t('prices.amountCount', { count: subscription.amount })}
-              validity={subscription.validity}
-            />
-          </StyledCard>
-        ))}
+          )}
       </Stack>
       <Stack spacing={0.5}>
         <Typography fontWeight="bold" fontSize="1.15em" textAlign="center">
