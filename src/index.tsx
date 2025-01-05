5ce4af91bd9a7a46f14a6b493490621548435c39
Roman Popov <popov.r.k@yandex.ru>
2025-01-05 13:39:13 +0500

    Tune yandex metrica

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
@@ -18,3 +19,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+try {
+  // @ts-ignore
+  window.ym(99409979, 'init', {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true,
+  });
+} catch (error) {
+  console.log('ym not found');
+  console.log({ error });
+}
